fe5b832b228b4ff59403893f5b03df1b8e876960
Mohit Kumar <mohitkuma8818@gmail.com>
2026-04-28 12:51:05 +0530

    updated project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,17 @@ function Header() {
     toast("Logged out successfully"); 
     }
     
-  
+    console.log(activeUser);
     
   return (
     <header>
-    <div className='header-logout flex justify-between items-center mx-auto! my-5!  py-3! bg-[#1F232A] rounded-[10px] text-white max-w-292.5'>
-      <div className="employee-info flex items-center gap-2 md:gap-5">
+    <div className='header-logout flex justify-between items-center px-2! mx-auto! my-5! md:px-6! py-3! bg-[#1F232A] rounded-[10px] text-white max-w-292.5'>
+      <div className="employee-info flex items-center gap-5">
         <div className="profile-logo max-w-17.5 max-h-17.5 rounded-full overflow-hidden">
           <img src={activeUser.currentUser.profilePicture} alt={activeUser.currentUser.name} className='w-full h-full object-cover' />
         </div>
         <div className="name-designation">
-          <h1 className=''><span className='user-name font-semibold text-[16px]! md:text-[24px]!'>{activeUser?.currentUser?.name}</span></h1>
+          <h1 className=''><span className='user-name font-semibold '>{activeUser?.currentUser?.name}</span></h1>
           <p className='designation text-[14px] bg-transparent bg-clip-text bg-linear-to-r from-white to-gray-400'>{activeUser?.currentUser?.designtion}</p>
         </div>
       </div>
